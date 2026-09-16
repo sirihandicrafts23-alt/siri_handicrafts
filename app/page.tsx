ef6594bdecbefa6ba350a4af2b8d33ea3b15ff2e
vijaysarma1007 <svmsarma53@gmail.com>
2026-09-16 20:56:41 +0530

    fixed text color

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ export default function Page() {
   return (
     <div className="flex min-h-svh p-6">
       <div className="bg-amber-100">
-        <h1>Siri Handicrafts - In progress </h1>
+        <h1 className="text-black font-semibold">Siri Handicrafts - In progress </h1>
       </div>
     </div>
   )
